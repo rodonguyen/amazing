ca14f4153c44c9613b094f27a1fb62059c09f715
Rodo <rodo@aervision.com>
2023-09-09 20:29:57 +1000

    Added life-progress bar, updated content, color

--- src/pages/TimeIHaveLeft.tsx
+++ src/pages/TimeIHaveLeft.tsx
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from 'react';
 
 const TimeIHaveLeft = () => {
+	const [percentage, setPercentage] = useState(0);
 	const [dob, setDob] = useState(new Date('2001-01-01'));
 	const [timeLeft, setTimeLeft] = useState({
 		years: 0,
 		months: 0,
 		days: 0,
 	});
+	const currentDate = new Date();
+
+	const calculatePercentage = (monthsLeft) => {
+		return ((1 - monthsLeft / (80 * 12)) * 100).toFixed(1)
+	}
+
 
 	const handleDobChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		const newDob = new Date(event.target.value);
@@ -22,7 +29,6 @@ const TimeIHaveLeft = () => {
 	};
 
 	const calculateTimeLeft = () => {
-		const currentDate = new Date();
 		const deathDate = new Date(dob);
 		deathDate.setFullYear(dob.getFullYear() + 80);
 
@@ -33,8 +39,11 @@ const TimeIHaveLeft = () => {
 		const yearsLeft = 80 - (currentDate.getFullYear() - dob.getFullYear());
 		const monthsLeft = calculateTotalMonths(deathDate, currentDate);
 		const daysLeft = Math.floor(differenceMs / (1000 * 60 * 60 * 24));
-		console.log(monthsLeft, daysLeft);
 
+		const tempPercentage = calculatePercentage(monthsLeft)
+		setPercentage(tempPercentage + '%')
+		const progressBar = document.getElementById("life-progress");
+		progressBar.style.width = `${tempPercentage}%`;
 		setTimeLeft({ years: yearsLeft, months: monthsLeft, days: daysLeft });
 	};
 
@@ -50,22 +59,38 @@ const TimeIHaveLeft = () => {
 			<input
 				type='date'
 				name='dob'
-				min='1800-01-01'
-				max='2100-01-01'
+				min='1950-01-01'
+				max='2030-01-01'
 				defaultValue='2001-01-01'
 				onChange={handleDobChange}
 				required
 			/>
 			<br></br>
-			{timeLeft.years > 0 && (
-				<>
-					<p>If you can live up to 80 years old, then...</p>
-					<p>
-						time amount you have left is: <b>~{timeLeft.years} years</b> OR{' '}
-						<b>~{timeLeft.months} months</b> OR <b>~{timeLeft.days} days</b>
-					</p>
-				</>
-			)}
+			<>
+				<p>If you can live up to 80 years old, assumingly don't get any genetec adjustments or have your body frozen, and survive through maybe a few more financial crises, socialeconomic revolutions, artificial intelligent impact, cancers, epidemics, then...</p>
+				<p>
+					the amount of time you have left is: <b>~{timeLeft.years} years</b> OR{' '}
+					<b>~{timeLeft.months} months</b> OR <b>~{timeLeft.days} days</b>
+				</p>
+			</>
+
+			{// Add case of more than 80yo 
+			}
+
+			{/* Progress bar */}
+			<div className="progress-container">
+				<div className="progress-bar" id="life-progress">
+					<div className="progress-text">{percentage}</div>
+				</div>
+			</div>
+
+			{/*Drawings pointing at some important milestone: 18, 60, */}
+
+			{/*Some sources about what to do with life, optimise life
+			- HieuTV cuoc doi 1 nguoi
+			- */}
+
+
 		</div>
 	);
 };

ddcbfa60f3d65b570b45acea21b1abea4c75b037
Rodo <rodonguyendd@gmail.com>
2023-12-28 17:56:29 +1000

    chore

--- src/pages/App.js
+++ src/pages/App.js
@@ -5,7 +5,6 @@ import NotFound from "./NotFound.js";
 import "../stylesheets/App.css";
 
 import { Outlet, BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState, useEffect } from "react";
 
 function App() {
   const AppConstantElements = (

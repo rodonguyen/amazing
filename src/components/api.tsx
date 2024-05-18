80f19d65cf1ef081aa1e35bf432c8f502557c376
Rodo <rodonguyendd@gmail.com>
2024-05-18 22:44:33 +1000

    fix: stupid outdated api url

--- src/components/api.tsx
+++ src/components/api.tsx
@@ -7,7 +7,7 @@ export type Birthday = {
 
 // const HAPPY_BIRTHDAY_API = process.env.REACT_APP_API_URL;
 // const HAPPY_BIRTHDAY_API = "http://localhost:3000/birthday";
-const HAPPY_BIRTHDAY_API = "https://homie-birthday-nest-api-rodonguyen-rodonguyens-projects.vercel.app/birthday";
+const HAPPY_BIRTHDAY_API = "https://homie-birthday-nest-api.vercel.app/birthday";
 
 export const fetchTodayBirthdays = (
   code: string = "rodo8888",
